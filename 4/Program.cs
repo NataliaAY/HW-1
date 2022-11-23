08df70cf6c254f01baef149d43c0c4d0ad4ed9cf
Natalia <averegina@male.ru>
2022-11-23 21:32:18 +0300

    решена задача 4

---
+++ 4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine ("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int i=0;
+Console.Write($"чётные числа от 1 до {number}:");
+while (i <number)
+{
+i+=1;
+if(i %2==0)
+{
+Console.WriteLine($"{i}");  
+}
+}
+
+
+         
